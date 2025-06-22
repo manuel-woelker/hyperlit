8713bf2ab27c4faeb6d6a6e685e4b39f6e915098
Manuel Woelker <github@manuel.woelker.org>
2025-06-22 16:21:06 +0200

    feat(release): release automation

--- scripts/build_release_package.sh
+++ scripts/build_release_package.sh
@@ -14,12 +14,16 @@ echo "Tag/Version: $VERSION"
 
 cargo build -p $PROJECT_NAME --release --target $TARGET --locked
 
+PACKAGE_NAME="$PROJECT_NAME-$VERSION-$TARGET"
+
 if [[ $TARGET == *"windows"* ]]; then
   echo "Creating zip file"
-  rm -f "$PROJECT_NAME-$TARGET.zip"
-  zip -j -r "$PROJECT_NAME-$VERSION-$TARGET.zip" "./target/$TARGET/release/$PROJECT_NAME.exe"
+  rm -f "$PACKAGE_NAME.zip"
+  #zip -j -r "$PROJECT_NAME-$VERSION-$TARGET.zip" "./target/$TARGET/release/$PROJECT_NAME.exe"
+  # Use power + Compressarchive instead for better windows compatibility
+  powershell -command "Compress-Archive -Path './target/$TARGET/release/$PROJECT_NAME.exe' -DestinationPath $PACKAGE_NAME.zip -Force"
 else
   echo "Creating tar.gz file"
-  rm -f "$PROJECT_NAME-$TARGET.tar.gz"
-  tar -zcvf "$PROJECT_NAME-$VERSION-$TARGET.tar.gz" "./target/$TARGET/release/$PROJECT_NAME"
+  rm -f "$PACKAGE_NAME.tar.gz"
+  tar -zcvf "$PACKAGE_NAME.tar.gz" "./target/$TARGET/release/$PROJECT_NAME"
 fi
