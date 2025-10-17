d602a5f911a12b6a622bb4144704f21e91e2dbbf
Manuel Woelker <manuel.woelker+jj@gmail.com>
2025-10-17 20:38:06 +0200

    fix(ci): make dummy index.html output

--- cli/src/hyperlit-cli.rs
+++ cli/src/hyperlit-cli.rs
@@ -3,6 +3,9 @@ use clap::Parser;
 use hyperlit_base::logging::init_logging;
 use hyperlit_base::result::HyperlitResult;
 use log::info;
+use std::fs::create_dir_all;
+use std::io::Write;
+use std::path::Path;
 
 pub mod arguments;
 
@@ -23,6 +26,9 @@ fn main() -> HyperlitResult<()> {
         Some(HyperlitCliCommands::Init {}) => todo!(),
         Some(HyperlitCliCommands::Watch {}) => todo!(),
         None => {
+            create_dir_all(Path::new("output"))?;
+            let mut index_file = std::fs::File::create("output/index.html")?;
+            index_file.write_all(b"<h1>hyperlit</h1>")?;
             //            let mut runner = Runner::new()?;
             //            runner.run()
         }
