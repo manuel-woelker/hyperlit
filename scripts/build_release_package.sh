a6e08198ef30820d65362a186f25746241e7df0e
Manuel Woelker <github@manuel.woelker.org>
2026-01-17 12:39:30 +0100

    feat(ci): Make node executable

--- scripts/build_release_package.sh
+++ scripts/build_release_package.sh
@@ -27,11 +27,12 @@ esac
 ./tool-tool$EXE_EXT --download
 # On linux make node exe executable
 
-if [[ $OSTYPE == linux* ]]; then
+echo OSTYPE: $OSTYPE
+uname -s
+if [ "$(uname -s)" = "Linux" ]; then
   echo "Making node executable"
   chmod +x ./.tool-tool/v2/cache/node-*-linux/bin/node
 fi
-
 
 # Build the UI
 UI_ZIP_FILE="./target/ui.zip"
